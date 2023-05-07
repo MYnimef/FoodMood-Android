18ab8dff23fb39c1d71f624637444fcf8c75315c
Ivan <73215129+MYnimef@users.noreply.github.com>
2023-05-08 02:03:25 +0300

    myicon change

--- app/src/main/java/com/mynimef/foodmood/presentation/elements/MyIcon.kt
+++ app/src/main/java/com/mynimef/foodmood/presentation/elements/MyIcon.kt
@@ -1,15 +1,17 @@
 package com.mynimef.foodmood.presentation.elements
 
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 
 @Composable
 fun MyIcon(
     modifier: Modifier = Modifier,
     drawableId: Int,
+    tint: Color = LocalContentColor.current
 ) {
     Icon (
         modifier = modifier,
