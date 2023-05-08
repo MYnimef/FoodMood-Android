9c1a33c8bfd28a9204ee2bd6cba884259e9ac205
Ivan <73215129+MYnimef@users.noreply.github.com>
2023-05-08 19:22:35 +0300

    home screen update

--- app/src/main/java/com/mynimef/foodmood/presentation/screens/trainer/HomeScreen.kt
+++ app/src/main/java/com/mynimef/foodmood/presentation/screens/trainer/HomeScreen.kt
@@ -1,11 +1,60 @@
 package com.mynimef.foodmood.presentation.screens.trainer
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.mynimef.foodmood.presentation.theme.FoodMoodTheme
 
 @Composable
 fun HomeScreen() {
     val viewModel: HomeViewModel = viewModel()
-    Text("home")
+    Column() {
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(bottomEnd = 8.dp))
+                .width(150.dp)
+                .height(45.dp)
+                .background(color = Color.White),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth(0.7f),
+                text = "31 января",
+                maxLines = 1,
+                textAlign = TextAlign.Center
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            modifier = Modifier
+                .fillMaxWidth(),
+            text = "home",
+            maxLines = 1,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun HomeScreenPreview() {
+    FoodMoodTheme {
+        HomeScreen()
+    }
 }
