943a60406f3437fee15b31db90222ebccda519db
Ivan <73215129+MYnimef@users.noreply.github.com>
2023-05-08 21:49:38 +0300

    correct date display

--- app/src/main/java/com/mynimef/foodmood/presentation/elements/MyDate.kt
+++ app/src/main/java/com/mynimef/foodmood/presentation/elements/MyDate.kt
@@ -1,9 +1,9 @@
-package com.mynimef.foodmood.presentation.screens.trainer
+package com.mynimef.foodmood.presentation.elements
 
+import android.content.res.Resources
+import android.text.format.DateFormat
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
@@ -17,20 +17,30 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.mynimef.foodmood.presentation.elements.MyDate
+import androidx.core.os.ConfigurationCompat
 import com.mynimef.foodmood.presentation.theme.FoodMoodTheme
 
+
 @Composable
-fun HomeScreen() {
-    val viewModel: HomeViewModel = viewModel()
-    Column {
-        MyDate()
-        Spacer(modifier = Modifier.height(20.dp))
+fun MyDate() {
+    val locale = ConfigurationCompat.getLocales(Resources.getSystem().configuration)
+    val date = DateFormat.format(
+        if (locale.toLanguageTags() == "ru-RU") "d MMMM" else "MMMM, d",
+        System.currentTimeMillis()
+    )
+
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(bottomEnd = 8.dp))
+            .width(150.dp)
+            .height(45.dp)
+            .background(color = Color.White),
+        contentAlignment = Alignment.Center
+    ) {
         Text(
             modifier = Modifier
-                .fillMaxWidth(),
-            text = "home",
+                .fillMaxWidth(0.7f),
+            text = date.toString(),
             maxLines = 1,
             textAlign = TextAlign.Center
         )
@@ -39,8 +49,8 @@ fun HomeScreen() {
 
 @Preview
 @Composable
-private fun HomeScreenPreview() {
+private fun MyDatePreview() {
     FoodMoodTheme {
-        HomeScreen()
+        MyDate()
     }
 }
