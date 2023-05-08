e3e9fd8d618202d3a548148f899b226ca6dd2878
Ivan <73215129+MYnimef@users.noreply.github.com>
2023-05-08 11:56:39 +0300

    login screen update

--- app/src/main/java/com/mynimef/foodmood/presentation/screens/shared/LoginScreen.kt
+++ app/src/main/java/com/mynimef/foodmood/presentation/screens/shared/LoginScreen.kt
@@ -2,8 +2,11 @@ package com.mynimef.foodmood.presentation.screens.shared
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -12,6 +15,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -21,38 +25,65 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.mynimef.foodmood.R
 import com.mynimef.foodmood.presentation.elements.MyIcon
 import com.mynimef.foodmood.presentation.theme.FoodMoodTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(navigateTo: (route: String) -> Unit) {
     val viewModel: LoginViewModel = viewModel()
 
     val login = remember { mutableStateOf("") }
     val password = remember { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier
+            .background(color = MaterialTheme.colorScheme.background)
+            .fillMaxSize()
+    ) {
+        CenterElements(
+            navigateTo = navigateTo,
+            login = login,
+            password = password
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun CenterElements(
+    navigateTo: (route: String) -> Unit,
+    login: MutableState<String>,
+    password: MutableState<String>
+) {
     val passwordVisible = rememberSaveable { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
-            .background(color = MaterialTheme.colorScheme.background)
-            .fillMaxSize(),
+            .fillMaxSize()
+            .padding(32.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(stringResource(R.string.welcome))
         TextField(
+            modifier = Modifier
+                .fillMaxWidth(),
             value = login.value,
             label = { Text(stringResource(R.string.email)) },
             singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
             onValueChange = {
                 login.value = it
             }
         )
         TextField(
+            modifier = Modifier
+                .fillMaxWidth(),
             value = password.value,
             label = { Text(stringResource(R.string.password)) },
             singleLine = true,
@@ -69,13 +100,18 @@ fun LoginScreen(navigateTo: (route: String) -> Unit) {
                 }
             }
         )
-        Text(text = "Нет аккаунта?")
+        Text(
+            modifier = Modifier
+                .fillMaxWidth(),
+            textAlign = TextAlign.Right,
+            text = stringResource(R.string.signup)
+        )
         Button(
             onClick = {
                 navigateTo("trainer")
             }
         ) {
-            Text(stringResource(R.string.login))
+            Text(stringResource(R.string.signin))
         }
     }
 }
