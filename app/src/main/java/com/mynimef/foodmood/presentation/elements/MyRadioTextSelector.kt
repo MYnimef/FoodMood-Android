19ae68aecb2f4bd9f6ba9725654eceb55c0b8301
Ivan <73215129+MYnimef@users.noreply.github.com>
2023-05-20 15:29:21 +0300

    added preview for myradiotextbutton

--- app/src/main/java/com/mynimef/foodmood/presentation/elements/MyRadioTextSelector.kt
+++ app/src/main/java/com/mynimef/foodmood/presentation/elements/MyRadioTextSelector.kt
@@ -11,7 +11,9 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.mynimef.foodmood.presentation.theme.FoodMoodTheme
 
 @Composable
 fun MyRadioTextSelector(
@@ -36,6 +38,16 @@ fun MyRadioTextSelector(
                 style = MaterialTheme.typography.titleMedium,
                 color = if (selected) MaterialTheme.colorScheme.primaryContainer else MaterialTheme.colorScheme.onPrimaryContainer
             )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun MyRadioTextSelector() {
+    FoodMoodTheme {
+        MyRadioTextSelector(text = "sample", selected = true) {
+
         }
     }
 }
