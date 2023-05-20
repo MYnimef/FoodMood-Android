58e1a43510d8f7ab6d909887e8db409a4a13025b
Ivan <73215129+MYnimef@users.noreply.github.com>
2023-05-20 12:09:54 +0300

    added stateflow to signup viewmodel

--- app/src/main/java/com/mynimef/foodmood/presentation/screens/shared/SignupViewModel.kt
+++ app/src/main/java/com/mynimef/foodmood/presentation/screens/shared/SignupViewModel.kt
@@ -1,6 +1,37 @@
 package com.mynimef.foodmood.presentation.screens.shared
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class SignupViewModel: ViewModel() {
+
+    private val _name = MutableStateFlow("")
+    val name: StateFlow<String> = _name.asStateFlow()
+
+    private val _login = MutableStateFlow("")
+    val login: StateFlow<String> = _login.asStateFlow()
+
+    private val _password = MutableStateFlow("")
+    val password: StateFlow<String> = _password.asStateFlow()
+
+    private val _repeatPassword = MutableStateFlow("")
+    val repeatPassword: StateFlow<String> = _repeatPassword.asStateFlow()
+
+    fun setName(value: String) {
+        _name.value = value
+    }
+
+    fun setLogin(value: String) {
+        _login.value = value
+    }
+
+    fun setPassword(value: String) {
+        _password.value = value
+    }
+
+    fun setRepeatPassword(value: String) {
+        _repeatPassword.value = value
+    }
 }
