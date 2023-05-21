27fce939a7c7896494e86ffebe898df57b1c66fd
Ivan <73215129+MYnimef@users.noreply.github.com>
2023-05-21 19:54:47 +0300

    client track

--- app/src/main/java/com/mynimef/foodmood/data/models/requests/SignUpRequest.kt
+++ app/src/main/java/com/mynimef/foodmood/data/models/requests/SignUpRequest.kt
@@ -3,8 +3,11 @@ package com.mynimef.foodmood.data.models.requests
 import com.google.gson.annotations.SerializedName
 
 data class SignUpRequest(
-    @SerializedName("email") val email: String,
-    @SerializedName("password") val password: String,
     @SerializedName("name") val name: String,
+    @SerializedName("email") val email: String,
+    @SerializedName("track_food") val trackFood: Boolean,
+    @SerializedName("track_water") val trackWater: Boolean,
+    @SerializedName("track_weight") val trackWeight: Boolean,
+    @SerializedName("password") val password: String,
     @SerializedName("device") val device: String,
 )
