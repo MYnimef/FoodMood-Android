d677bc67a840e4495b98e94508568249726bb1e3
tultsovaa <nastyatyltsova@gmail.com>
2023-05-23 08:31:02 +0300

    update create

--- app/src/main/java/com/mynimef/foodmood/data/models/enums/ETypeEmotion.kt
+++ app/src/main/java/com/mynimef/foodmood/data/models/enums/ETypeEmotion.kt
@@ -1,9 +1,11 @@
 package com.mynimef.foodmood.data.models.enums
 
-enum class ETypeEmotion {
-    VERY_BAD,
-    BAD,
-    NORMAL,
-    GOOD,
-    GREAT,
+import com.mynimef.foodmood.R
+
+enum class ETypeEmotion(val icon: Int) {
+    VERY_BAD(icon = R.drawable.ic_mood_very_bad),
+    BAD(icon = R.drawable.ic_mood_bad),
+    NORMAL(icon = R.drawable.ic_mood_normal),
+    GOOD(icon = R.drawable.ic_mood_good),
+    GREAT(icon = R.drawable.ic_mood_great),
 }
