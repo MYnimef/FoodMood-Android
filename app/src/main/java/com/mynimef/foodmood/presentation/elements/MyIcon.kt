08c4568e6eb8580aef312214b6dfb91c67e9c066
Ivan <73215129+MYnimef@users.noreply.github.com>
2023-05-23 13:18:03 +0300

    one more fix

--- app/src/main/java/com/mynimef/foodmood/presentation/elements/MyIcon.kt
+++ app/src/main/java/com/mynimef/foodmood/presentation/elements/MyIcon.kt
@@ -1,7 +1,6 @@
 package com.mynimef.foodmood.presentation.elements
 
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -11,7 +10,6 @@ import androidx.compose.ui.res.painterResource
 fun MyIcon(
     modifier: Modifier = Modifier,
     drawableId: Int,
-    tint: Color = LocalContentColor.current
 ) {
     Icon (
         modifier = modifier,
