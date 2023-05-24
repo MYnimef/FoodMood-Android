3465301594497d2531f012dedc5d4c70b76ce29e
Ivan <73215129+MYnimef@users.noreply.github.com>
2023-05-24 16:00:08 +0300

    updated card DAO

--- app/src/main/java/com/mynimef/foodmood/data/repository/dao/CardDao.kt
+++ app/src/main/java/com/mynimef/foodmood/data/repository/dao/CardDao.kt
@@ -1,10 +1,24 @@
 package com.mynimef.foodmood.data.repository.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import com.mynimef.foodmood.data.models.database.CardEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CardDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(account: CardEntity): Long
 
+    @Query("DELETE FROM card")
+    fun deleteAll()
+
+    @Transaction
+    @Query("SELECT * FROM card")
+    fun getAll(): Flow<List<CardEntity>>
 
 }
