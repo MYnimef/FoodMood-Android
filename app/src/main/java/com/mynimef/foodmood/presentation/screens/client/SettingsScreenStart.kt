ec75e3f754798686c598f430e8ff7fcf3b683761
tultsovaa <nastyatyltsova@gmail.com>
2023-05-30 00:19:43 +0300

    update settings icons

--- app/src/main/java/com/mynimef/foodmood/presentation/screens/client/SettingsScreenStart.kt
+++ app/src/main/java/com/mynimef/foodmood/presentation/screens/client/SettingsScreenStart.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mynimef.foodmood.R
 import com.mynimef.foodmood.data.models.enums.ENavigationSettings
+import com.mynimef.foodmood.extensions.noRippleClickable
 import com.mynimef.foodmood.presentation.elements.MyIcon
 import com.mynimef.foodmood.presentation.theme.FoodMoodTheme
 
@@ -59,16 +60,19 @@ private fun CenterElements(
                 .fillMaxWidth()
                 .padding(bottom = 20.dp)
         ) {
-            IconButton(
-                modifier = Modifier.align(Alignment.CenterVertically),
-                onClick = { navigateTo(ENavigationSettings.USER_INFO) }) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .noRippleClickable (
+                        onClick = { navigateTo(ENavigationSettings.USER_INFO) })) {
                 MyIcon(drawableId = R.drawable.ic_user)
             }
             TextButton(
                 onClick = { navigateTo(ENavigationSettings.USER_INFO) },) {
                 Text(stringResource(R.string.user_settings),
                     modifier = Modifier.align(Alignment.CenterVertically),
-                    style = MaterialTheme.typography.titleMedium)
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer)
             }
         }
         Row(
@@ -76,16 +80,19 @@ private fun CenterElements(
                 .fillMaxWidth()
                 .padding(bottom = 20.dp)
         ) {
-            IconButton(
-                modifier = Modifier.align(Alignment.CenterVertically),
-                onClick = { navigateTo(ENavigationSettings.PREFERENCES)}) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .noRippleClickable (
+                        onClick = { navigateTo(ENavigationSettings.PREFERENCES) })) {
                 MyIcon(drawableId = R.drawable.ic_preference)
             }
             TextButton(
                 onClick = { navigateTo(ENavigationSettings.PREFERENCES) },) {
                 Text(stringResource(R.string.prefrences),
                     modifier = Modifier.align(Alignment.CenterVertically),
-                    style = MaterialTheme.typography.titleMedium)
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer)
             }
         }
         Row(
@@ -93,16 +100,19 @@ private fun CenterElements(
                 .fillMaxWidth()
                 .padding(bottom = 20.dp)
         ) {
-            IconButton(
-                modifier = Modifier.align(Alignment.CenterVertically),
-                onClick = { navigateTo(ENavigationSettings.NOTIFICATION)}) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .noRippleClickable (
+                        onClick = { navigateTo(ENavigationSettings.NOTIFICATION) })) {
                 MyIcon(drawableId = R.drawable.ic_notification)
             }
             TextButton(
                 onClick = { navigateTo(ENavigationSettings.NOTIFICATION) },) {
                 Text(stringResource(R.string.notifications),
                     modifier = Modifier.align(Alignment.CenterVertically),
-                    style = MaterialTheme.typography.titleMedium)
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer)
             }
         }
         Row(
@@ -119,7 +129,8 @@ private fun CenterElements(
                 onClick = { },) {
                 Text(stringResource(R.string.language),
                     modifier = Modifier.align(Alignment.CenterVertically),
-                    style = MaterialTheme.typography.titleMedium)
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer)
             }
         }
         Row(
@@ -127,16 +138,19 @@ private fun CenterElements(
                 .fillMaxWidth()
                 .padding(bottom = 20.dp)
         ) {
-            IconButton(
-                modifier = Modifier.align(Alignment.CenterVertically),
-                onClick = { }) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .noRippleClickable (
+                        onClick = { })) {
                 MyIcon(drawableId = R.drawable.ic_error)
             }
             TextButton(
                 onClick = { },) {
                 Text(stringResource(R.string.error),
                     modifier = Modifier.align(Alignment.CenterVertically),
-                    style = MaterialTheme.typography.titleMedium)
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer)
             }
         }
         Row(
@@ -144,16 +158,19 @@ private fun CenterElements(
                 .fillMaxWidth()
                 .padding(bottom = 20.dp)
         ) {
-            IconButton(
-                modifier = Modifier.align(Alignment.CenterVertically),
-                onClick = { }) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .noRippleClickable (
+                        onClick = {  })) {
                 MyIcon(drawableId = R.drawable.ic_idea)
             }
             TextButton(
                 onClick = { },) {
                 Text(stringResource(R.string.idea),
                     modifier = Modifier.align(Alignment.CenterVertically),
-                    style = MaterialTheme.typography.titleMedium)
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer)
             }
         }
     }
