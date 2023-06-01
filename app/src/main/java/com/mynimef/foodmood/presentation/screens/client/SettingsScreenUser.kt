d159554808c60fb6c9dbad12f3ba3dea68d439b2
Ivan <73215129+MYnimef@users.noreply.github.com>
2023-06-02 01:33:21 +0300

    fix

--- app/src/main/java/com/mynimef/foodmood/presentation/screens/client/SettingsScreenUser.kt
+++ app/src/main/java/com/mynimef/foodmood/presentation/screens/client/SettingsScreenUser.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -50,7 +49,6 @@ fun SettingsScreenUser() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun CenterElements(
     name: MutableState<String>,
@@ -159,7 +157,7 @@ private fun CenterElements(
 
 @Preview
 @Composable
-fun UserInfoScreenPrev() {
+private fun SettingsScreenUserPreview() {
     FoodMoodTheme {
         SettingsScreenUser()
     }
