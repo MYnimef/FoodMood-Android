16606865184715f27ef9c07975d03cf053014411
Ivan <73215129+MYnimef@users.noreply.github.com>
2023-07-16 19:53:29 +0300

    fixed reports screen

--- app/src/main/java/com/mynimef/foodmood/data/models/enums/ETypePeriod.kt
+++ app/src/main/java/com/mynimef/foodmood/data/models/enums/ETypePeriod.kt
@@ -1,6 +1,8 @@
 package com.mynimef.foodmood.data.models.enums
 
-enum class ETypePeriod(val period: Int) {
-    DAYS_7(period = 7),
-    DAYS_31(period = 31),
+import com.mynimef.foodmood.R
+
+enum class ETypePeriod(val period: Int, val label: Int, ) {
+    DAYS_7(period = 7, label = R.string.days_7),
+    DAYS_31(period = 31, label = R.string.days_31),
 }
