9700f398ff5fcf26feb310321ce761e74df44bf6
Ivan <73215129+MYnimef@users.noreply.github.com>
2023-07-16 22:35:46 +0300

    sign out ability

--- app/src/main/java/com/mynimef/foodmood/presentation/screens/client/SetUpAccountViewModel.kt
+++ app/src/main/java/com/mynimef/foodmood/presentation/screens/client/SetUpAccountViewModel.kt
@@ -1,20 +1,19 @@
 package com.mynimef.foodmood.presentation.screens.client
 
 import androidx.lifecycle.ViewModel
-import com.mynimef.foodmood.data.models.enums.ENavClientMain
 import com.mynimef.foodmood.data.repository.Repository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
-class SettingsViewModel: ViewModel() {
+class SetUpAccountViewModel: ViewModel() {
 
     private var job: Job? = null
 
-    fun navigateTo(nav: ENavClientMain) = with(Repository) {
-        job = CoroutineScope(Dispatchers.Main).launch {
-            clientNavMain.emit(nav)
+    fun signOut() = with(Repository) {
+        job = CoroutineScope(Dispatchers.IO).launch {
+            signOut()
         }
     }
 
