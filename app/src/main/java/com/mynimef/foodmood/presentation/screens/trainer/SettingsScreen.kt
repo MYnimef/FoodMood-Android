32847d78733b934e0b7ec2cfe5b826a8793d5bcc
Ivan <73215129+MYnimef@users.noreply.github.com>
2023-10-07 16:20:28 +0300

    some trainer code update

--- app/src/main/java/com/mynimef/foodmood/presentation/screens/trainer/SettingsScreen.kt
+++ app/src/main/java/com/mynimef/foodmood/presentation/screens/trainer/SettingsScreen.kt
@@ -2,10 +2,18 @@ package com.mynimef.foodmood.presentation.screens.trainer
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.mynimef.foodmood.presentation.theme.FoodMoodTheme
 
 @Composable
 fun SettingsScreen() {
     val viewModel: SettingsViewModel = viewModel()
     Text("settings")
+}
+
+@Preview
+@Composable
+private fun SettingsScreenPreview() = FoodMoodTheme {
+    SettingsScreen()
 }
