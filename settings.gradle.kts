c3bc83b5a036922181fa9468a480e544146d165c
Ivan <73215129+MYnimef@users.noreply.github.com>
2023-10-08 03:46:23 +0300

    converted gradle to gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,8 +10,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url 'https://jitpack.io' }
+        maven { setUrl("https://jitpack.io") }
     }
 }
+
 rootProject.name = "FoodMood"
-include ':app'
+include(":app")
