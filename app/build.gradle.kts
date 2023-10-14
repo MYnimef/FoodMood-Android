7ad61b7a74f7aeaa35c413059a8eee28e5d97c35
Ivan <73215129+MYnimef@users.noreply.github.com>
2023-10-14 03:34:29 +0300

    updated deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,15 +61,15 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
 
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.09.02"))
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation(platform("androidx.compose:compose-bom:2023.10.00"))
 
-    implementation("androidx.compose.ui:ui:1.5.2")
-    implementation("androidx.compose.ui:ui-graphics:1.5.2")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.2")
+    implementation("androidx.compose.ui:ui:1.5.3")
+    implementation("androidx.compose.ui:ui-graphics:1.5.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.3")
     implementation("androidx.compose.material3:material3:1.1.2")
-    implementation("androidx.compose.material:material:1.5.2")
-    implementation("androidx.navigation:navigation-compose:2.7.3")
+    implementation("androidx.compose.material:material:1.5.3")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
 
     implementation("androidx.core:core-splashscreen:1.0.1")
 
@@ -87,10 +87,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.09.02"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.2")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.3")
 
 }
